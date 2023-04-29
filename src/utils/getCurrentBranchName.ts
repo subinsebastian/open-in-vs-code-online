5746b842c67e0688d5f1dc498a862f5de98d2f43
Subin Sebastian <subin.sebastian457@gmail.com>
2023-04-29 23:15:09 +0530

    created: getCurrentBranchName

--- src/utils/getCurrentBranchName.ts
+++ src/utils/getCurrentBranchName.ts
@@ -1,1 +1,18 @@
-export function getCurrentBranchName() {}
+import { exec } from 'child_process';
+import { Deferred } from './Deferred';
+
+export function getCurrentBranchName() {
+	const deferred = new Deferred<string>();
+	exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
+		if (err) {
+			// handle your error
+			deferred.reject(err);
+		}
+
+		if (typeof stdout === 'string') {
+			deferred.resolve(stdout?.trim());
+		}
+	});
+
+	return deferred.promise;
+}
