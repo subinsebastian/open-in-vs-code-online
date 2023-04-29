14677ab6f6a156198ce38582217f603e33f57c62
Subin Sebastian <subin.sebastian457@gmail.com>
2023-04-29 23:31:42 +0530

    created: `getNameOfCurrentFileInEditorView`

--- src/utils/getNameOfCurrentFileInEditorView.ts
+++ src/utils/getNameOfCurrentFileInEditorView.ts
@@ -1,1 +1,9 @@
-export function getNameOfCurrentFileInEditorView() {}
+import * as vscode from 'vscode';
+import { Deferred } from './Deferred';
+
+export function getNameOfCurrentFileInEditorView() {
+	const deferred = new Deferred<string | null>();
+
+	deferred.resolve(vscode.window.activeTextEditor?.document?.fileName || null);
+	return deferred.promise;
+}
