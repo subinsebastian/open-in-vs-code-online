ad1d6513cc1a099ef74292e11cf8f85e7eba72b4
Subin Sebastian <subin.sebastian457@gmail.com>
2023-04-30 09:18:15 +0530

    deleted: unnecessary commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,6 @@
 import { Command } from '../types';
-import { listGitBranchesCommand } from './listGitBranches';
 import { openInVSCodeOnlineCommand } from './openInVSCodeOnline';
-import { showGitOriginUrlCommand } from './showGitOriginUrl';
 
 export function getAllCommands(): Array<Command> {
-	return [openInVSCodeOnlineCommand(), listGitBranchesCommand(), showGitOriginUrlCommand()];
+	return [openInVSCodeOnlineCommand()];
 }
