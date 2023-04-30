862e6e8c377e6988e1ef24a611f17aa8f5bea18e
Subin Sebastian <subin.sebastian457@gmail.com>
2023-04-30 15:43:28 +0530

    Squashed commit of the following:
    commit 8d86300c0b527dcf11380e6d450926aec6a88a9f
    Author: Subin Sebastian <subin.sebastian457@gmail.com>
    Date:   Sun Apr 30 15:43:10 2023 +0530
    
        added: script
    
    commit 743c5dbc57bb3c4f49c69b1caf8d69c438eacdb3
    Author: Subin Sebastian <subin.sebastian457@gmail.com>
    Date:   Sun Apr 30 15:31:13 2023 +0530
    
        fixed: link for vscode.dev
    
    commit d97080f29d3374cbf451b7b95c73182cf0b4fcec
    Author: Subin Sebastian <subin.sebastian457@gmail.com>
    Date:   Sun Apr 30 13:47:42 2023 +0530
    
        added: publish scripts

--- src/constants/configs.constant.ts
+++ src/constants/configs.constant.ts
@@ -1,6 +1,6 @@
 export const CONFIGS = {
 	defaultVSCodeOnlineEnvironment: {
-		'vscode.dev': 'https://vscode.dev',
+		'vscode.dev': 'https://vscode.dev/github',
 		'github.dev': 'https://github.dev',
 	},
 };
